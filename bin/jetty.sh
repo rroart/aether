0ccffe3d7ae7f5532936339fd9d348b471cb40b5
Roar Thronæs <roart@nvg.ntnu.no>
2012-01-10 21:49:35 +0000

    java heap oom memory fix

--- bin/jetty.sh
+++ bin/jetty.sh
@@ -1,1 +1,1 @@
-JAVA_OPT="-Xms2048m -Xmx2048m" JAVA_HOME=/opt/jdk1.7.0 ~/usr/apache-maven-3.0.3/bin/mvn -Duser.showsecret=1 -Psecret jetty:run 
+MAVEN_OPTS="-Xms2048m -Xmx2048m" JAVA_OPT="-Xms2048m -Xmx2048m" JAVA_HOME=/opt/jdk1.7.0 ~/usr/apache-maven-3.0.3/bin/mvn -Duser.showsecret=1 -Psecret jetty:run 
