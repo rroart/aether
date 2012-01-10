0eca7f2646b2abfb5083c36c8924f0fa205ff357
Roar Thronæs <roart@nvg.ntnu.no>
2012-01-10 12:56:42 +0000

    Initial revision

---
+++ src/main/java/roart/dir/Traverse.java
@@ -0,0 +1,184 @@
+package roart.dir;
+
+import java.util.Set;
+import java.util.HashMap;
+import java.io.*;
+import roart.content.*;
+import roart.search.*;
+import roart.model.*;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Traverse {
+
+    private static Log log = LogFactory.getLog("Traverse");
+
+    public static HashSet<String> doList (String dirname, HashMap<String, HashSet<String>> dirset) throws Exception {
+	HashSet<String> retset = new HashSet<String>();
+	HashSet<String> md5set = new HashSet<String>();
+	File dir = new File(dirname);
+	File listDir[] = dir.listFiles();
+	//log.info("dir " + dirname);
+	//log.info("listDir " + listDir.length);
+	for (int i = 0; i < listDir.length; i++) {
+	    String filename = listDir[i].getAbsolutePath();
+	    if (filename.length() > 250) {
+		log.info("Too large filesize " + filename);
+		continue;
+	    }
+	    //log.info("file " + filename);
+	    if (listDir[i].isDirectory()) {
+		//log.info("isdir " + filename);
+		retset.addAll(doList(filename, dirset));
+	    } else {
+		//log.info("retset " + filename);
+		retset.add(filename);
+		//Reader reader = new ParsingReader(parser, stream, ...);
+		File plainFile = new File(filename);
+		Files files = Files.ensureExistence(filename);
+		//files.setTouched(new Boolean(true));
+		if (files.getMd5() == null) {
+		    try {
+			FileInputStream fis = new FileInputStream( new File(filename));
+			String md5 = org.apache.commons.codec.digest.DigestUtils.md5Hex( fis );
+			files.setMd5(md5);
+			log.info("adding md5 file " + filename);
+		    } catch (Exception e) {
+			log.info("Error: " + e.getMessage());
+			log.error("Exception", e);
+		    }
+		}
+		md5set.add(files.getMd5());
+	    }
+	}
+	dirset.put(dirname, md5set);
+	//log.info("retsize " + retset.size());
+	return retset;
+    }
+
+    public static ArrayList<String> index() throws Exception {
+	ArrayList<String> retlist = new ArrayList<String>();
+	log.info("here");
+	List<Files> files = Files.getAll();
+	List<Index> indexes = Index.getAll();
+	log.info("sizes " + files.size() + " " + indexes.size());
+	HashMap<String, String> filesMapMd5 = new HashMap<String, String>();
+	HashMap<String, String> filesMapFilename = new HashMap<String, String>();
+	for (Files file : files) {
+	    filesMapMd5.put(file.getMd5(), file.getFilename());
+	    filesMapFilename.put(file.getFilename(), file.getMd5());
+	}
+	HashMap<String, Boolean> indexMap = new HashMap<String, Boolean>();
+	for (Index index : indexes) {
+	    indexMap.put(index.getMd5(), index.getIndexed());
+	}
+	for (String md5 : filesMapMd5.keySet()) {
+	    if (md5 == null) {
+		log.info("md5 should not be null");
+		continue;
+	    }
+	    Boolean indexed = indexMap.get(md5);
+	    if (indexed != null) {
+		if (indexed.booleanValue()) {
+		    continue;
+		}
+	    }
+	    String filename = filesMapMd5.get(md5);
+	    log.info("indexing " + filename);
+	    Index index = Index.ensureExistence(md5);
+	    //InputStream stream = null;
+	    int size = 0;
+	    size = doTika(filename, filename, md5, index, retlist);
+	    int limit = mylimit(filename);
+	    if (false && size <= limit) {
+		String lowercase = filename.toLowerCase();
+		if (lowercase.endsWith(".djvu") || lowercase.endsWith(".djv")) {
+		    String[] env = { filename };
+		    String output = execute("/usr/bin/djvutxt", env);
+		}
+		if (lowercase.endsWith(".epub")) {
+		    String[] env = { filename, "/tmp/t.txt" };
+		    String output = execute("/usr/bin/ebook-convert", env);
+		}
+		if (lowercase.endsWith(".pdf")) {
+		    String[] env = { filename, "/tmp/t.txt" };
+		    String output = execute("/usr/bin/pdftotext", env);
+		}
+		size = doTika(filename, "/tmp/t.txt", md5, index, retlist);
+	    }
+	}
+	return retlist;
+    }
+
+    private static int doTika(String dbfilename, String filename, String md5, Index index, ArrayList<String> retlist) {
+	int size = 0;
+	try {
+	    TikaHandler tika = new TikaHandler();
+	    OutputStream outputStream = tika.process(filename);
+	    InputStream inputStream =new ByteArrayInputStream(((ByteArrayOutputStream) outputStream).toByteArray());
+	    size = ((ByteArrayOutputStream)outputStream).size();
+	    log.info("size1 " + size);
+	    int limit = mylimit(dbfilename);
+	    if (size > limit) {
+		size = SearchLucene.indexme("all", md5, inputStream);
+		index.setIndexed(new Boolean(true));
+	    } else {
+		log.info("Too small " + filename + " " + md5 + " " + size + " " + limit);
+		retlist.add(dbfilename + " " + md5 + " " + size);
+	    }
+	    log.info("size2 " + size);
+	    inputStream.close();
+	    outputStream.close();
+	} catch (Exception e) {
+	    log.info(e);
+	    log.error("Exception", e);
+	} finally {
+	    log.info("bla");
+	    //stream.close();            // close the stream
+	}
+	return size;
+    }
+
+    private static String execute(String filename, String[] env) {
+        Process proc = null;
+        try {
+	    proc = Runtime.getRuntime().exec(filename, env);
+            if (proc != null) proc.waitFor();
+        } catch (Exception e) {
+	    log.info("Exception" + e);
+	    log.error("Exception", e);
+        }
+	/*
+	StringBuilder buffer = new StringBuilder();
+	BufferedInputStream br = new BufferedInputStream(proc.getInputStream());
+	while (br.available() != 0) {
+	    buffer.append((char) br.read());
+	}
+	String res = buffer.toString().trim();
+	return res;
+	*/
+	return null;
+    }
+
+    private static int mylimit(String filename) {
+	String lowercase = filename.toLowerCase();
+	if (lowercase.endsWith(".pdf")) {
+	    return 4096;
+	}
+	if (lowercase.endsWith(".djvu") || lowercase.endsWith(".djv")) {
+	    return 4096;
+	}
+	if (lowercase.endsWith(".mp3")) {
+	    return 16;
+	}
+	return 4096;
+    }
+    
+}
---
+++ src/main/java/roart/model/HibernateUtil.java
@@ -0,0 +1,86 @@
+package roart.model;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
+import org.hibernate.annotations.Index;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.SessionFactory;
+import org.hibernate.HibernateException;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.AnnotationConfiguration;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import java.io.Serializable;
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+// dummy
+//import net.sf.ehcache.hibernate.EhCacheRegionFactory;
+
+public class HibernateUtil {
+
+    private static SessionFactory factory = null;
+    private static Session session = null;
+    private static Transaction transaction = null;
+
+    public static Session getCurrentSession() throws /*MappingException,*/ HibernateException, Exception {
+	return getHibernateSession();
+    }
+
+    public static Session currentSession() throws /*MappingException,*/ HibernateException, Exception {
+	return getHibernateSession();
+    }
+
+    public static Session getHibernateSession() throws /*MappingException,*/ HibernateException, Exception {
+	if (factory == null) {
+	    AnnotationConfiguration configuration = new AnnotationConfiguration();
+	    factory = configuration.configure().buildSessionFactory();
+	    //Object o = new net.sf.ehcache.hibernate.EhCacheRegionFactory();
+	}
+
+	if (session == null) {
+	    //Session sess = factory.openSession();
+	    session = factory.getCurrentSession();
+	}
+
+	if (transaction == null) {
+	    transaction = session.beginTransaction();
+	}
+
+	if (session != null) {
+	    if (!session.isOpen()) {
+		session = factory.openSession();
+	    }
+	}
+	return session;
+    }
+
+    public static void commit() throws /*MappingException,*/ HibernateException, Exception {
+	transaction.commit();
+	transaction = null;
+	session = null;
+    }
+
+    public static <T> List<T> convert(List l, Class<T> type) {
+        return (List<T>)l;
+    }
+
+}
