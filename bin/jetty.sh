9396555ad2c1522b5d24001f9be75c687b62c1f8
Roar Thronæs <roart@nvg.ntnu.no>
2012-01-12 19:57:50 +0000

    lower limit

--- bin/jetty.sh
+++ bin/jetty.sh
@@ -1,1 +1,1 @@
-MAVEN_OPTS="-Xms2048m -Xmx2048m" JAVA_OPT="-Xms2048m -Xmx2048m" JAVA_HOME=/opt/jdk1.7.0 ~/usr/apache-maven-3.0.3/bin/mvn -Duser.showsecret=1 -Psecret jetty:run 
+MAVEN_OPTS="-Xms1024m -Xmx1024m" JAVA_OPT="-Xms1024m -Xmx1024m" JAVA_HOME=/opt/jdk1.7.0 ~/usr/apache-maven-3.0.3/bin/mvn -Duser.showsecret=1 -Psecret jetty:run 
