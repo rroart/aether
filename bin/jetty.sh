e3d17829bdb93fa0acbf2b1c15813c1c80462aec
Roar Thronæs <roart@nvg.ntnu.no>
2012-01-14 17:03:45 +0000

    mem change

--- bin/jetty.sh
+++ bin/jetty.sh
@@ -1,1 +1,1 @@
-MAVEN_OPTS="-Xms1024m -Xmx1024m" JAVA_OPT="-Xms1024m -Xmx1024m" JAVA_HOME=/opt/jdk1.7.0 ~/usr/apache-maven-3.0.3/bin/mvn -Duser.showsecret=1 -Psecret jetty:run 
+JAVA_HOME=/opt/jdk1.7.0 ~/usr/apache-maven-3.0.3/bin/mvn -Duser.showsecret=1 -Psecret jetty:run 
