c675cae6f49e3afccca8bb020f0d47cac95882e5
Roar Thronæs <roart@nvg.ntnu.no>
2013-09-25 11:16:57 +0000

    another thread level for handling timeout, and then shutdown thread pool

--- src/main/java/roart/thread/TikaRunner.java
+++ src/main/java/roart/thread/TikaRunner.java
@@ -11,6 +11,8 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
@@ -29,33 +31,47 @@ public class TikaRunner implements Runnable {
     public void run() {
     	Map<Future<Object>, Date> map = new HashMap<Future<Object>, Date>();
     	int nThreads = Runtime.getRuntime().availableProcessors() / 4;
+    	int running = 0;
     	log.info("nthreads " + nThreads);
-    	ExecutorService executorService = Executors.newFixedThreadPool(nThreads);
+    	ThreadPoolExecutor /*ExecutorService*/ executorService = (ThreadPoolExecutor) Executors.newFixedThreadPool(nThreads);
 
     	while (true) {
     		long now = new Date().getTime();
     		List<Future> removes = new ArrayList<Future>();
     		for(Future<Object> task: map.keySet()) {
+     			if (task.isCancelled()) {
+    				log.info("cancelled and removing " + task + " " + map.size());
+    				removes.add(task);   
+    	            //FutureTask<Object> ft = (FutureTask<Object>) task;
+    	            //ft.run();
+    	            //ft.notify();
+    				continue;
+    			}
     			if (task.isDone()) {
     				log.info("removing " + task);
     				removes.add(task);    				
         			Date d = map.get(task);
         			if ( d != null) {
-        				log.info("timerStop " + (d.getTime() - now));
+        				log.info("timerStop " + (now - d.getTime()));
         			}
-    			}
-    			if (task.isCancelled()) {
-    				log.info("cancelled and removing " + task);
-    				removes.add(task);    				
+        			continue;
     			}
     			Date d = map.get(task);
-    			if ( d != null && (now - d.getTime()) < 10 * 60 * 1000) {
+    			if (true) { continue; }
+    			if ( d != null && (now - d.getTime()) < 100/*0*/ * 60 * 1/*0*/) {
     				continue;
     			}
+    			
+    			Queues.decTikas();
+	            log.error("timeout and removing " + task + " " + map.size());
+    	        boolean ok = task.cancel(true);
+    	        if (!ok) {
+    	        	log.error("canceled error");
+    	        }
+    	        /*    	        
     	        try {
-    	            // ok, wait for 600 seconds max
-    	            Object result = task.get(1, TimeUnit.MILLISECONDS);
-    	            log.info("does this get here finished and removing " + task);
+    	        	Object result = task.get(1, TimeUnit.MILLISECONDS);
+    	        	log.info("does this get here finished and removing " + task + " " + map.size());
     	            removes.add(task);
     	            Queues.decTikas();
     	        } catch (ExecutionException e) {
@@ -65,14 +81,29 @@ public class TikaRunner implements Runnable {
     	            log.error("timeout and removing " + task);
     	            removes.add(task);
     	            Queues.decTikas();
+        	        boolean ok = task.cancel(true);
+        	        if (!ok) {
+        	        	log.error("canceled error");
+        	        }
+    	            //Thread.currentThread().interrupt();    	            
+    	            //task.cancel(true);
+    	            //FutureTask<Object> ft = (FutureTask<Object>) task;
+    	            //ft.run();
     	        } catch (InterruptedException e) {
     	            log.error("interrupted", e);
     	        }	
+    	        */
     		}
     		for (Future<Object> key: removes) {
     			map.remove(key);
+    			running--;
     		}
-    		if (Queues.tikaQueue.isEmpty()) {
+    		if (false && removes.size() > 0) {
+    		log.info("active 0 " + executorService.getActiveCount());
+    		executorService.purge();
+    		log.info("active 1 " + executorService.getActiveCount());
+    		}
+   		if (Queues.tikaQueue.isEmpty()) {
     			try {
     				TimeUnit.SECONDS.sleep(1);
     			} catch (InterruptedException e) {
@@ -81,26 +112,69 @@ public class TikaRunner implements Runnable {
     			}
     			continue;
     		}
-    		Queues.queueStat();
+    		//Queues.queueStat();
     	
-    		for(int i = 0; i < Queues.tikaQueue.size(); i++) {
+    		for(int i = running; i < nThreads; i++) {
     			Callable<Object> callable = new Callable<Object>() {
-    				public Object call() throws Exception {
-    					Traverse.doTika();
+    				public Object call() /* throws Exception*/ {
+    					try {
+    						doTikaTimeout();
+    					} catch (Throwable t) {
+    						//log.info("throwable");
+    					}
+    					finally {
+    						//log.info("myend");
+    					}
     					return null; //myMethod();
     				}	
     			};	
  
     			Future<Object> task = executorService.submit(callable);
     			map.put(task, new Date());
-    		}
+    			Queues.queueStat();
+    			running++;
+    			log.info("submit " + task + " " + running + " service count " + executorService.getActiveCount());
+    			log.info("queue " + executorService.getQueue());
+    			/*
+       			int num = executorService.prestartAllCoreThreads();
+       			log.info("num " + num);
+       			*/
+  		}
 			try {
-				TimeUnit.SECONDS.sleep(60);
-			} catch (InterruptedException e) {
+				//TimeUnit.SECONDS.sleep(60);
+			} catch (/*Interrupted*/Exception e) {
 				// TODO Auto-generated catch block
 				log.error("Exception", e);
 			}
     	}
+    }
+
+    public static String doTikaTimeout() {
+        Callable<Object> callable = new Callable<Object>() {
+            public Object call() throws Exception {
+                Traverse.doTika();
+                return null;
+            }
+        };
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
+        Future<Object> task = executorService.submit(callable);
+        Object result = null;
+        try {
+            // ok, wait for 600 seconds max
+            result = task.get(600, TimeUnit.SECONDS);
+            log.info("Finished with result: " + result);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        } catch (TimeoutException e) {
+            log.error("timeout...", e);
+            log.error("timeout and removing " + task);
+            Queues.decTikas();
+        } catch (InterruptedException e) {
+            log.error("interrupted", e);
+        }
+        executorService.shutdown();  
+        return (String) result;
     }
 
 }
