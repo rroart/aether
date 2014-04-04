3f35b36b80afea38eab3b83341610a83e64fb0ae
Roar Thronæs <roart@nvg.ntnu.no>
2014-04-04 11:29:18 +0000

    null check

--- src/main/java/roart/search/SearchLucene.java
+++ src/main/java/roart/search/SearchLucene.java
@@ -119,8 +119,10 @@ public class SearchLucene {
 	doc.add(new TextField(Constants.LANG, lang, Field.Store.YES));
 	}
 	doc.add(new TextField(Constants.NAME, i, Field.Store.NO));
-	log.info("with md " + metadata.toString());
-	doc.add(new StringField(Constants.METADATA, metadata.toString(), Field.Store.NO));
+	if (metadata != null) {
+	    log.info("with md " + metadata.toString());
+	    doc.add(new StringField(Constants.METADATA, metadata.toString(), Field.Store.NO));
+	}
 	Term term = new Term(Constants.TITLE, md5);
 	w.updateDocument(term, doc);
 	//w.addDocument(doc);
