75802a95cf6474143b46f95ae008d56cae82be1b
Roar Thronæs <roart@nvg.ntnu.no>
2014-10-21 16:09:50 +0200

    null check

--- src/main/java/roart/jpa/LocalFileSystem.java
+++ src/main/java/roart/jpa/LocalFileSystem.java
@@ -20,25 +20,24 @@ public class LocalFileSystem {
 		List<FileObject> foList = new ArrayList<FileObject>();
 		File dir = (File) f.object;
 		File listDir[] = dir.listFiles();
+		if (listDir != null) {
 		for (File file : listDir) {
 			FileObject fo = new FileObject(file);
 			foList.add(fo);
+		}
 		}
 		return foList;
 	}
 
 	public static boolean exists(FileObject f) {
-		// TODO Auto-generated method stub
 		return ((File) f.object).exists();
 	}
 
 	public static String getAbsolutePath(FileObject f) {
-		// TODO Auto-generated method stub
 		return ((File) f.object).getAbsolutePath();
 	}
 
 	public static boolean isDirectory(FileObject f) {
-		// TODO Auto-generated method stub
 		return ((File) f.object).isDirectory();
 	}
 
@@ -46,19 +45,16 @@ public class LocalFileSystem {
 		try {
 			return new FileInputStream( new File(getAbsolutePath(f)));
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			log.error("Exception", e);
 			return null;
 		}
 	}
 
 	public static FileObject get(String string) {
-		// TODO Auto-generated method stub
 		return new FileObject(new File(string));
 	}
 
 	public static FileObject getParent(FileObject f) {
-		// TODO Auto-generated method stub
 		String parent = ((File) f.object).getParent();
 		File file = new File(parent);
 		return new FileObject(file);
