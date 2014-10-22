e4d009a8e58758884554e1644e945ddfd0788cca
Roar Thronæs <roart@nvg.ntnu.no>
2014-10-22 18:17:41 +0200

    remove todo

--- src/main/java/roart/jpa/LocalFileSystemJpa.java
+++ src/main/java/roart/jpa/LocalFileSystemJpa.java
@@ -9,43 +9,36 @@ public class LocalFileSystemJpa extends FileSystemJpa {
 
 	@Override
 	public List<FileObject> listFiles(FileObject f) {
-		// TODO Auto-generated method stub
 	    return LocalFileSystem.listFiles(f);
 	}
 
 	@Override
 	public boolean exists(FileObject f) {
-		// TODO Auto-generated method stub
 	    return LocalFileSystem.exists(f);
 	}
 
 	@Override
 	    public String getAbsolutePath(FileObject f) {
-		// TODO Auto-generated method stub
 	    return LocalFileSystem.getAbsolutePath(f);
 	}
 
 	@Override
 	public boolean isDirectory(FileObject f) {
-		// TODO Auto-generated method stub
 		return LocalFileSystem.isDirectory(f);
 	}
 
 	@Override
 	public InputStream getInputStream(FileObject f) {
-		// TODO Auto-generated method stub
 		return LocalFileSystem.getInputStream(f);
 	}
 
 	@Override
 	public FileObject getParent(FileObject f) {
-		// TODO Auto-generated method stub
 		return LocalFileSystem.getParent(f);
 	}
 
 	@Override
 	public FileObject get(String string) {
-		// TODO Auto-generated method stub
 		return LocalFileSystem.get(string);
 	}
 
