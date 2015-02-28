5ec957efd83c8f18a2ec5158d5bdeec9a42cc90e
Roar Thronæs <roart@nvg.ntnu.no>
2015-02-28 08:22:13 +0100

    new version

--- bin/export.sh
+++ bin/export.sh
@@ -2,8 +2,8 @@ rsync -a ~/usr/data/h2/backup/ ~/usr/data/h2/backup.1
 
 cp -p ~/usr/data/h2/stuff.* ~/usr/data/h2/backup
 
-java -cp /home/roart/.m2/repository/com/h2database/h2/1.3.173/h2-1.3.173.jar org.h2.tools.Script -url jdbc:h2:~/usr/data/h2/stuff -user "" -script ~/usr/data/h2/backup/t.sql
+java -cp /home/roart/.m2/repository/com/h2database/h2/1.4.185/h2-1.4.185.jar org.h2.tools.Script -url jdbc:h2:~/usr/data/h2/stuff -user "" -script ~/usr/data/h2/backup/t.sql
 
 cd ~/usr/data/h2/
-java -cp /home/roart/.m2/repository/com/h2database/h2/1.3.173/h2-1.3.173.jar org.h2.tools.Recover
+java -cp /home/roart/.m2/repository/com/h2database/h2/1.4.185/h2-1.4.185.jar org.h2.tools.Recover
 mv stuff.h2.sql backup
