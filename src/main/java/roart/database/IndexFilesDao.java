67270c734372745f369e920c6e1b7d9ddcce7578
Roar Thronæs <roart@nvg.ntnu.no>
2015-03-01 12:52:54 +0100

    synchronized db access

--- src/main/java/roart/database/IndexFilesDao.java
+++ src/main/java/roart/database/IndexFilesDao.java
@@ -46,6 +46,7 @@ public class IndexFilesDao {
 	if (all.containsKey(md5)) {
 	    return all.get(md5);
 	}
+	synchronized(IndexFilesDao.class) {
 	IndexFiles i = indexFiles.getByMd5(md5);
 	if (i == null && create) {
 	    i = new IndexFiles(md5);
@@ -54,6 +55,7 @@ public class IndexFilesDao {
 	all.put(md5, i);
 	}
 	return i;
+	}
     }
 
     public static IndexFiles getByMd5(String md5) throws Exception {
@@ -68,28 +70,37 @@ public class IndexFilesDao {
 	if (md5 == null) {
 	    return null;
 	}
+	synchronized(IndexFilesDao.class) {
 	return indexFiles.getFilelocationsByMd5(md5);
+	}
     }
 
     public static IndexFiles getByFilenameNot(String filename) throws Exception {
 	String nodename = ControlService.nodename;
 	FileLocation fl = new FileLocation(nodename, filename);
+	synchronized(IndexFilesDao.class) {
 	return indexFiles.getByFilelocation(fl);
+	}
     }
 
     public static IndexFiles getByFilelocationNot(FileLocation fl) throws Exception {
+	synchronized(IndexFilesDao.class) {
 	return indexFiles.getByFilelocation(fl);
+	}
     }
 
     public static String getMd5ByFilename(String filename) throws Exception {
 	String nodename = ControlService.nodename;
 	FileLocation fl = new FileLocation(nodename, filename);
+	synchronized(IndexFilesDao.class) {
 	return indexFiles.getMd5ByFilelocation(fl);
+	}
     }
 
     public static List<IndexFiles> getAll() throws Exception {
 	//all.clear();
     	Set<String> allKeys = all.keySet();
+	synchronized(IndexFilesDao.class) {
 	List<IndexFiles> iAll = indexFiles.getAll();
 	for (IndexFiles i : iAll) {
 		if (allKeys.contains(i.getMd5())) {
@@ -98,16 +109,21 @@ public class IndexFilesDao {
 	    all.put(i.getMd5(), i);
 	}
 	return iAll;
+	}
     }
 
     public static Set<String> getAllMd5() throws Exception {
+	synchronized(IndexFilesDao.class) {
 	Set<String> md5All = indexFiles.getAllMd5();
 	return md5All;
+	}
     }
 
     public static Set<String> getLanguages() throws Exception {
+	synchronized(IndexFilesDao.class) {
     	Set<String> languages = indexFiles.getLanguages();
     	return languages;
+	}
         }
 
     /*
@@ -133,7 +149,9 @@ public class IndexFilesDao {
     public static void save(IndexFiles i) {
 	if (i.hasChanged()) {
 		try {
+		    synchronized(IndexFilesDao.class) {
 			indexFiles.save(i);
+		    }
 		    log.info("saving " + i.getMd5());
 	    	i.setUnchanged();
 		} catch (Exception e) {
@@ -165,7 +183,9 @@ public class IndexFilesDao {
 	}
 	//all.clear();
 	try {
+	    synchronized(IndexFilesDao.class) {
 	indexFiles.close();
+	    }
 	} catch (Exception e) {
 	    log.error(Constants.EXCEPTION, e);
     }
@@ -173,7 +193,9 @@ public class IndexFilesDao {
 
     public static void flush() {
     	try {
+	    synchronized(IndexFilesDao.class) {
 	indexFiles.flush();
+	    }
 		} catch (Exception e) {
 		    log.error(Constants.EXCEPTION, e);
 	    }
