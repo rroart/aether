7a182ad5490483d4955ed8b082b2ea96922736ae
Roar Thronæs <roart@nvg.ntnu.no>
2016-01-12 15:47:31 +0100

    nodeconfig serializing

--- src/main/java/roart/hcutil/HazelcastConfig.java
+++ src/main/java/roart/hcutil/HazelcastConfig.java
@@ -4,6 +4,7 @@ import roart.model.FileObject;
 import roart.model.ResultItem;
 import roart.queue.ClientQueueElement;
 import roart.queue.TraverseQueueElement;
+import roart.config.NodeConfig;
 
 import com.hazelcast.config.Config;
 import com.hazelcast.config.SerializerConfig;
@@ -17,12 +18,16 @@ public class HazelcastConfig {
         SerializerConfig sc2 = new SerializerConfig()
         .setImplementation(new ClientQueueElementSerializer())
         .setTypeClass(ClientQueueElement.class);
+        SerializerConfig sc3 = new SerializerConfig()
+        .setImplementation(new NodeConfigSerializer())
+        .setTypeClass(NodeConfig.class);
         SerializerConfig sc4 = new SerializerConfig()
         .setImplementation(new ResultItemSerializer())
         .setTypeClass(ResultItem.class);
         Config config = new Config();
         config.getSerializationConfig().addSerializerConfig(sc);
         config.getSerializationConfig().addSerializerConfig(sc2);
+        config.getSerializationConfig().addSerializerConfig(sc3);
         config.getSerializationConfig().addSerializerConfig(sc4);
        return config;
     }
---
+++ src/main/java/roart/hcutil/NodeConfigSerializer.java
@@ -0,0 +1,41 @@
+package roart.hcutil;
+
+import com.hazelcast.nio.ObjectDataInput;
+import com.hazelcast.nio.ObjectDataOutput;
+import com.hazelcast.nio.serialization.StreamSerializer;
+
+import java.beans.XMLDecoder;
+import java.beans.XMLEncoder;
+import java.io.*;
+
+import roart.config.NodeConfig;
+
+public class NodeConfigSerializer implements StreamSerializer<NodeConfig> {
+
+  @Override
+      public int getTypeId() {
+      return 3;
+  }
+
+  @Override
+      public void write( ObjectDataOutput out, NodeConfig object ) throws IOException {
+      ByteArrayOutputStream bos = new ByteArrayOutputStream();
+      XMLEncoder encoder = new XMLEncoder( bos );
+      encoder.writeObject( object );
+      encoder.close();
+      out.write( bos.toByteArray() );
+      //System.out.println(bos.toString());
+  }
+
+  @Override
+      public NodeConfig read( ObjectDataInput in ) throws IOException {
+      InputStream inputStream = (InputStream) in;
+      XMLDecoder decoder = new XMLDecoder( inputStream );
+      return (NodeConfig) decoder.readObject();
+  }
+
+  @Override
+      public void destroy() {
+  }
+}
+
