0b624145ef90f1559715445e1850e57b4e0fc822
Roar Thronæs <roart@nvg.ntnu.no>
2016-05-16 08:35:57 +0200

    wrapper fix

--- src/main/java/org/apache/lucene/search/similarities/DefaultSimilarity.java
+++ src/main/java/org/apache/lucene/search/similarities/DefaultSimilarity.java
@@ -2,11 +2,15 @@ package org.apache.lucene.search.similarities;
 
 public class DefaultSimilarity {
     ClassicSimilarity sim;
-    DefaultSimilarity() {
+    public DefaultSimilarity() {
         sim = new ClassicSimilarity();
     }
     
-    float tf(float freq) {
+    public float tf(float freq) {
         return sim.tf(freq);
+    }
+
+    public float idf(long docFreq, long numDocs) {
+        return sim.idf(docFreq, numDocs);
     }
 }
