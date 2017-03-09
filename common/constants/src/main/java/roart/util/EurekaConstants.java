e0080ade1595ecb1610e5245ac11aa120189b681
Roar Thronæs <roart@nvg.ntnu.no>
2017-03-09 16:53:25 +0100

    eureka

---
+++ common/constants/src/main/java/roart/util/EurekaConstants.java
@@ -0,0 +1,12 @@
+package roart.util;
+
+public class EurekaConstants {
+	public static final String LUCENE = "LUCENE";
+	public static final String SOLR = "SOLR";
+	public static final String ELASTIC = "ELASTIC";
+	public static final String CONSTRUCTOR = "constructor";
+	public static final String DELETE = "delete";
+	public static final String INDEX = "index";
+	public static final String SEARCH = "search";
+	public static final String SEARCHMLT = "searchmlt";
+}
