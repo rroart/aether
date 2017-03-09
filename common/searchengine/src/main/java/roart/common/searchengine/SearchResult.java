9c2bd30ed6944390a59fde7c0e83443ce16b4229
Roar Thronæs <roart@nvg.ntnu.no>
2017-03-09 16:50:35 +0100

    better data types

--- common/searchengine/src/main/java/roart/common/searchengine/SearchResult.java
+++ common/searchengine/src/main/java/roart/common/searchengine/SearchResult.java
@@ -1,10 +1,12 @@
 package roart.common.searchengine;
 
+import java.util.List;
+
 public class SearchResult {
     public String md5;
-    public String score;
+    public float score;
     public String lang;
-    public String highlights;
+    public String[] highlights;
     public String display;
-    public String metadata;
+    public List<String> metadata;
 }
