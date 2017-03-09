8d12d39fdcf0199a90bce2614915f27265b48631
Roar Thronæs <roart@nvg.ntnu.no>
2017-03-09 16:54:34 +0100

    add/move constants

---
+++ common/constants/src/main/java/roart/util/Constants.java
@@ -0,0 +1,38 @@
+package roart.util;
+
+public class Constants {
+
+public static final String EXCEPTION = "Exception";
+public static final String ERROR = "Error";
+
+public static final String USER = "user";
+public static final String NONE = "none";
+public static final String ADMIN = "admin";
+public static final String ADDR = "addr";
+
+public static final String AETHER = "aether";
+public static final String LOCK = "lock";
+public static final String NODES = "nodes";
+public static final String REFRESH = "refresh";
+public static final String RECONFIG = "reconfig";
+public static final String QUEUE = "queue";
+public static final String DB = "db";
+
+public static final String ZKDELIMITER = "-";
+
+public static final String FILESCONTENTTYPE = "Files-Content-Type";
+
+public static final String INDEXCOUNT = "indexcount";
+public static final String TRAVERSEQUEUE = "traversequeue";
+public static final String TRAVERSECOUNT = "traversecount";
+public static final String FILESETNEWID = "filesetnewid";
+public static final String NOTFOUNDSETID = "notfoundsetid";
+public static final String RETLISTID = "retlistid";
+public static final String RETNOTLISTID = "retnotlistid";
+public static final String ZOOKEEPER = "zookeeper";
+public static final String CURATOR = "curator";
+public static final String HAZELCAST = "hazelcast";
+public static final String GLOBALLOCK = "globallock";
+public static final String FILESTODOSETID = "filestodoset";
+
+}
