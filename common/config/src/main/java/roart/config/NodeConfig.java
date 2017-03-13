80d283d6cffca2726f10ce0350ab5e595c284e69
Roar Thronæs <roart@nvg.ntnu.no>
2017-03-13 07:29:05 +0100

    changing language to be array

--- common/config/src/main/java/roart/config/NodeConfig.java
+++ common/config/src/main/java/roart/config/NodeConfig.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public class NodeConfig {
     public enum Config { REINDEXLIMIT, INDEXLIMIT, FAILEDLIMIT, OTHERTIMEOUT, TIKATIMEOUT, MLTCOUNT, MLTMINTF, MLTMINDF }
 
-    public String languages = null;
+    public String[] languages = null;
     
     public String[] dirlist = null;
     public String[] dirlistnot = null;
