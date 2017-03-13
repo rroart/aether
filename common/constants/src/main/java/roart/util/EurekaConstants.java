202ec833b3279bc9e7ed6f45964c3fdcc0626dc6
Roar Thronæs <roart@nvg.ntnu.no>
2017-03-13 07:33:56 +0100

    machine learning. deconstructor

--- common/constants/src/main/java/roart/util/EurekaConstants.java
+++ common/constants/src/main/java/roart/util/EurekaConstants.java
@@ -4,9 +4,15 @@ public class EurekaConstants {
 	public static final String LUCENE = "LUCENE";
 	public static final String SOLR = "SOLR";
 	public static final String ELASTIC = "ELASTIC";
+	public static final String SPARKML = "SPARKML";
+	public static final String MAHOUTMR = "MAHOUTMR";
+	public static final String MAHOUTSPARK = "MAHOUTSPARK";
+	public static final String OPENNLP = "OPENNLP";
 	public static final String CONSTRUCTOR = "constructor";
+	public static final String DECONSTRUCTOR = "deconstructor";
 	public static final String DELETE = "delete";
 	public static final String INDEX = "index";
 	public static final String SEARCH = "search";
 	public static final String SEARCHMLT = "searchmlt";
+	public static final String CLASSIFY = "classify";
 }
