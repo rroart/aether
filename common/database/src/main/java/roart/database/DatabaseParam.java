0ad0ca23bea3b9119ca4c1c2dbe5c16a44e6aa34
Roar Thronæs <roart@nvg.ntnu.no>
2017-12-14 23:53:45 +0100

    Jackson handling subclasses.

--- common/database/src/main/java/roart/database/DatabaseParam.java
+++ common/database/src/main/java/roart/database/DatabaseParam.java
@@ -1,7 +1,20 @@
 package roart.database;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
 import roart.config.NodeConfig;
 
+@JsonTypeInfo(  
+        use = JsonTypeInfo.Id.NAME,  
+        include = JsonTypeInfo.As.PROPERTY,  
+        property = "_class")  
+@JsonSubTypes({  
+    @Type(value = DatabaseConstructorParam.class, name = "roart.model.DatabaseConstructorParam"),  
+    @Type(value = DatabaseFileLocationParam.class, name = "roart.model.DatabaseFileLocationParam"),  
+    @Type(value = DatabaseIndexFilesParam.class, name = "roart.model.DatabaseIndexFilesParam"),  
+    @Type(value = DatabaseMd5Param.class, name = "roart.model.DatabaseMd5Param") })  
 public abstract class DatabaseParam {
     public String nodename;
     public NodeConfig conf;
