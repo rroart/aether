442e3c6c33e27e1766f87d3d05c328a4a380baac
Roar Thronæs <roart@nvg.ntnu.no>
2017-12-14 23:46:03 +0100

    Need getter/setter.

--- common/filesystem/src/main/java/roart/filesystem/FileSystemFileObjectResult.java
+++ common/filesystem/src/main/java/roart/filesystem/FileSystemFileObjectResult.java
@@ -4,4 +4,12 @@ import roart.model.FileObject;
 
 public class FileSystemFileObjectResult extends FileSystemResult {
     FileObject[] fileObject;
+
+    public FileObject[] getFileObject() {
+        return fileObject;
+    }
+
+    public void setFileObject(FileObject[] fileObject) {
+        this.fileObject = fileObject;
+    }
 }
