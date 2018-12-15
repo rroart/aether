7c8e8ee6c9af30e802fff83d0904e10ebd54aacf
Roar Thronæs <roart@nvg.ntnu.no>
2018-12-15 12:10:50 +0100

    Upgrade jetty.

--- bin/service-simple.sh
+++ bin/service-simple.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd ../lib
 xterm -e "java -jar aether-servicemanager-simple-0.10-SNAPSHOT.jar $1" &
-xterm -e "java -jar jetty-runner-9.4.10.RC1.jar --port 8280 aether-web-0.10-SNAPSHOT.war" &
+xterm -e "java -jar jetty-runner-9.4.12.v20180830.jar --port 8280 aether-web-0.10-SNAPSHOT.war" &
 
