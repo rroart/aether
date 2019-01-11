c64b247fb0720bfc4a91d82fc680fbe66dd66704
Roar Thronæs <roart@nvg.ntnu.no>
2019-01-11 10:25:21 +0100

    Deprecate jetty-runner, use micro service.

--- bin/service-docker.sh
+++ bin/service-docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash                                                                     
 cd ../lib
 xterm -e "java -jar aether-servicemanager-docker-0.10-SNAPSHOT.jar $1" &
-xterm -e "java -jar jetty-runner-9.4.12.v20180830.jar --port 8280 aether-web-0.10-SNAPSHOT.war" &
+xterm -e "java -jar aether-web-0.10-SNAPSHOT.jar" &
 
--- bin/service-simple-debug.sh
+++ bin/service-simple-debug.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd ../lib
 xterm -e "java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=18100,suspend=n -jar aether-servicemanager-simple-0.10-SNAPSHOT.jar $1" &
-xterm -e "java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=18101,suspend=n -jar jetty-runner-9.4.12.v20180830.jar --port 8280 aether-web-0.10-SNAPSHOT.war" &
+xterm -e "java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=18101,suspend=n -jar aether-web-0.10-SNAPSHOT.jar" &
 
