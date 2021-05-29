0f91a4701d519bfe6b3876886cf6eeb3f88aa660
Roar Thronæs <roart@nvg.ntnu.no>
2021-05-29 23:59:34 +0200

    Communication (I2).

--- common/util/src/main/java/module-info.java
+++ common/util/src/main/java/module-info.java
@@ -11,4 +11,8 @@ module common.util {
     requires common.constants;
     requires java.xml;
     requires slf4j.api;
+    requires commons.math3;
+    requires org.apache.commons.lang3;
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.core;
 }
