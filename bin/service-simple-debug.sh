9c5e3d0f08b74d14209a05951cb339a6c2fc7b6a
Roar Thronæs <roart@nvg.ntnu.no>
2021-10-09 20:52:44 +0200

    Temp fix for Servicemanager (I12).

--- bin/service-simple-debug.sh
+++ bin/service-simple-debug.sh
@@ -2,5 +2,6 @@
 cd ../lib
 xterm -e "java -jar aether-eureka-0.10-SNAPSHOT.jar" &
 xterm -e "java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=18100,suspend=n -jar aether-servicemanager-simple-0.10-SNAPSHOT.jar $1" &
+xterm -e "java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=18102,suspend=n -jar aether-core-0.10-SNAPSHOT.jar $1" &
 xterm -e "java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=18101,suspend=n -jar aether-web-0.10-SNAPSHOT.jar" &
 
