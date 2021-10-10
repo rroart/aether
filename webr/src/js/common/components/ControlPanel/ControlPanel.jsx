f82ded2919dd0cd85e6d9962d9b4e3226490b9f6
Roar Thronæs <roart@nvg.ntnu.no>
2021-10-10 22:47:54 +0200

    Temp commit, builds and displays, web with React (I14).

--- webr/src/js/common/components/ControlPanel/ControlPanel.jsx
+++ webr/src/js/common/components/ControlPanel/ControlPanel.jsx
@@ -2,23 +2,28 @@ import React, { PureComponent } from 'react';
 
 import { Client, ConvertToSelect } from '../util'
 import Select from 'react-select';
-import { DropdownButton, MenuItem, ButtonToolbar, Nav, Navbar, NavItem, FormControl } from 'react-bootstrap';
-const options = [
-  { value: 'chocolate', label: 'Chocolate' },
-  { value: 'strawberry', label: 'Strawberry' },
-  { value: 'vanilla', label: 'Vanilla' }
-];
-
-const options2 = [
-  { label: 'chocolate' },
-  { label: 'strawberry' },
-  { label: 'vanilla' }
-];
+import { DropdownButton, MenuItem, Button, ButtonToolbar, Nav, Navbar, NavItem, Form, FormControl } from 'react-bootstrap';
+import type { ServiceParam } from '../../types/main'
 
 class ControlPanel extends PureComponent {
   constructor() {
     super();
 }
+
+    filesystemlucenenew(event, md5checknew, props) {
+    console.log(event);
+    console.log(event.value);
+    console.log(event.target);
+    console.log(event.target.value);
+    console.log(props);
+	var param = new ServiceParam();
+	param.config = this.props.config;
+	param.function = "FILESYSTEMLUCENENEW";
+	param.add = event.value;
+	param.md5checknew = md5checknew;
+	param.webpath = "filesystemlucenenew";
+    //props.control([ param ]);
+  }
 
   render() {
 const { main } = this.props;
@@ -30,7 +35,33 @@ const { main } = this.props;
       return (
 	  <div>
 	      <h2>Hei</h2>
-      </div>
+	      <Navbar>
+		  <Navbar.Header>
+		      <Navbar.Brand>
+			  <a href="#home">Indexing</a>
+		      </Navbar.Brand>
+		  </Navbar.Header>
+		  <Nav>
+		      <NavItem eventKey={1} href="#">
+			  <Button bsStyle="primary" onClick={ (e) => this.filesystemlucenenew(e, false, this.props) }>Index filesystem new items</Button>
+		      </NavItem>
+		      <NavItem eventKey={2} href="#">
+			  <Form onSubmit={ (e) => this.filesystemlucenenew(e, false, this.props)}>
+			      <FormControl
+				  type="text"
+				  placeholder="New items path"/>
+			  </Form>
+		      </NavItem>
+		      <NavItem eventKey={3} href="#">
+			  <FormControl
+			      type="text"
+			      placeholder="Changed md5 path"
+			      onSubmit={ (e) => this.filesystemlucenenew(e, false, this.props) }
+			  />
+		      </NavItem>
+		  </Nav>
+              </Navbar>
+	  </div>
     );
   }
 /*
--- webr/src/js/common/components/Search/Search.jsx
+++ webr/src/js/common/components/Search/Search.jsx
@@ -25,15 +25,61 @@ class Search extends PureComponent {
   render() {
 const { main } = this.props;
       //this.bardvd = new SearchBar('dvd');
-      console.log(Object.keys(main))
-      console.log(main.config);
-      //console.log(main.config.get("searchengine.lucene"));
-      //console.log(main.config.get("searchengine.lucene[@enable]"));
+      //console.log(Object.keys(main))
+      //console.log(main.config);
+      //console.log(typeof main.config);
+      //console.log(Object.keys(main.config));
+      //console.log(main.config.keys());
+      //console.log(main.config.text);
+      //console.log(main.config.get("_root"));
+      //console.log(main.config.get("conf"));
+      //console.log(main.config.get("text"));
+      //console.log(main.config.conf);
+      //console.log(Object.keys(main.config.get('configValueMap')));
+      //console.log(main.config.configValueMap);
+      //console.log(main.config['configValueMap']);
+      //console.log(main.config.get('configValueMap'));
+      //console.log(main.config.get('configValueMap').get("searchengine.lucene"));
+      //console.log(main.config.get('configValueMap').get("searchengine.lucene[@enable]"));
+      //console.log(main.config.get('configValueMap').get("database.cassandra[@enable]"));
+      //console.log(main.config.get('configValueMap')["database.cassandra[@enable]"]);
+      //console.log(main.config.get('configValueMap').get("searchengine.lucene[@enable]"));
+      let Searchbars=<h2>Waiting for config</h2>;
+      if (!!main.config) {
+	  const dolucene = main.config.get('configValueMap').get("searchengine.lucene[@enable]");
+	  const dosolr = main.config.get('configValueMap').get("searchengine.solr[@enable]");
+	  const doelastic = main.config.get('configValueMap').get("searchengine.elastic[@enable]");
+	  if (dolucene) {
+	      Searchbars=<div>
+			     <SearchBar text='Search standard' type='0'/>
+			     <SearchBar text='Search analyzing' type='1'/>
+			     <SearchBar text='Search complexphrase' type='2'/>
+			     <SearchBar text='Search extendable' type='3'/>
+			     <SearchBar text='Search multi' type='4'/>
+			     <SearchBar text='Search surround' type='5'/>
+			     <SearchBar text='Search classic' type='6'/>
+			     <SearchBar text='Search simple' type='7'/>
+			 </div>;
+	  }
+	  if (dosolr) {
+	      Searchbars=<div>
+			     <SearchBar text='Search default' type='0'/>
+			     <SearchBar text='Search lucene' type='1'/>
+			     <SearchBar text='Search complexphrase' type='2'/>
+			     <SearchBar text='Search surround' type='3'/>
+			     <SearchBar text='Search simple' type='4'/>
+			 </div>;
+	  }
+	  if (doelastic) {
+	      Searchbars=<div>
+			     <SearchBar text='Search' type='0'/>
+			 </div>;
+	  }
+      }
       return (
-      <div>
-	  <SearchBar text='Search standard' type='0'/>
-	  <SearchBar text='Search analyzing' type='1'/>
-      </div>
+	  <div>
+	      { Searchbars }
+	      </div>
     );
   }
 /*
