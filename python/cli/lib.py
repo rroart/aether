6d81211b3ebfc774540f3b5059619e58b992d74f
Roar Thronæs <roart@nvg.ntnu.no>
2021-11-04 19:55:44 +0100

    A little more for text client (I25).

--- python/cli/lib.py
+++ python/cli/lib.py
@@ -2,6 +2,8 @@
 import readline
 readline.set_auto_history(False)
 import request
+import matplotlib.pyplot as plt
+import multiprocessing as mp
 
 # class SearchEngineParam:
 #     def __init__(self, webpath, nodename, conf):
@@ -15,7 +17,11 @@ import request
 #         def.str = str
 #         def.searchtype = searchtype
 
-def search(search, type):
+def search(search, type = "0"):
+        import multiprocessing as mp
+        mp.Process(target=searchP, args=(search, type)).start()
+
+def searchP(search, type = "0"):
         #SearchEngineSearchParam param = SearchEngineSearchParam();
         #param.conf = getConfig();
         #param.str = search;
@@ -26,8 +32,20 @@ def search(search, type):
         print(response.json())
         print(response.json()['list'])
         l = response.json()['list']
-        for i in l:
-            for j in i:
-                print(j['items'])
-
+        val1 = l[0][0]['items']
+        val2 = [ 1 + i for i in range(len(l[0]) - 1 ) ]
+        #for i in l:
+        #    for j in i:
+        #        print(j['items'])
+        val3 = [ l[0][i]['items'] for i in range(1, len(l[0])) ]
+        fig, ax = plt.subplots() 
+        ax.set_axis_off()
+        table = ax.table( 
+                cellText = val3,  
+                rowLabels = val2,  
+                colLabels = val1, 
+                cellLoc ='left',  
+                loc ='upper left')
+        table.auto_set_font_size(False)
+        plt.show()
 
