f8d13d803cc7d86acd3bbc4de78a88f4d41dcd77
Roar Thronæs <roart@nvg.ntnu.no>
2021-12-18 22:04:27 +0100

    Temp fix for Servicemanager (I12).

--- bin/service-simple.sh
+++ bin/service-simple.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd ../lib
+xterm -e "java -jar aether-eureka-0.10-SNAPSHOT.jar" &
 xterm -e "java -jar aether-servicemanager-simple-0.10-SNAPSHOT.jar $1" &
+xterm -e "java -jar aether-core-0.10-SNAPSHOT.jar $1" &
 xterm -e "java -jar aether-web-0.10-SNAPSHOT.jar" &
 
