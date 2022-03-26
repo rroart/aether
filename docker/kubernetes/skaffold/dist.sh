019b91f0998621e4b592062fb6de82913c3af170
Roar Thronæs <roart@nvg.ntnu.no>
2022-03-26 22:28:40 +0100

    Add the rest of the services. Handle different environments, for automation (I4).

--- docker/kubernetes/skaffold/dist.sh
+++ docker/kubernetes/skaffold/dist.sh
@@ -1,1 +1,26 @@
+export DISTLIB=distribution/target/aether-distribution-0.10-SNAPSHOT-bin/aether-distribution-0.10-SNAPSHOT/lib
+
+export CALIBREJAR=aether-calibre-0.10-SNAPSHOT.jar
+export CASSANDRAJAR=aether-cassandra-0.10-SNAPSHOT.jar
+export COREJAR=aether-core-0.10-SNAPSHOT.jar
+export DJVUTXTJAR=aether-djvutxt-0.10-SNAPSHOT.jar
+export DYNAMODBJAR=aether-dynamodb-0.10-SNAPSHOT.jar
+export ELASTICJAR=aether-elastic-0.10-SNAPSHOT.jar
+export HBASEJAR=aether-hbase-0.10-SNAPSHOT.jar
+export HDFSJAR=aether-hdfs-0.10-SNAPSHOT.jar
+export HIBERNATEJAR=aether-hibernate-0.10-SNAPSHOT.jar
+export LOCALJAR=aether-local-0.10-SNAPSHOT.jar
+export LUCENEJAR=aether-lucene-0.10-SNAPSHOT.jar
+export MAHOUTMRJAR=aether-mahout-mr-0.10-SNAPSHOT.jar
+export MAHOUTSPARKJAR=aether-mahout-spark-0.10-SNAPSHOT.jar
+export OPENNLPJAR=aether-opennlp-0.10-SNAPSHOT.jar
+export PDFTOTEXTJAR=aether-pdftotext-0.10-SNAPSHOT.jar
+export S3JAR=aether-s3-0.10-SNAPSHOT.jar
+export SOLRJAR=aether-solr-0.10-SNAPSHOT.jar
+export SPARKJAR=aether-spark-ml-0.10-SNAPSHOT.jar
+export SWIFTJAR=aether-swift-0.10-SNAPSHOT.jar
+export TIKAJAR=aether-tika-0.10-SNAPSHOT.jar
+export WEBJAR=aether-web-0.10-SNAPSHOT.jar
+export WVTEXTJAR=aether-wvtext-0.10-SNAPSHOT.jar
+
 export WEBRDOCROOT=distribution/target/aether-distribution-0.10-SNAPSHOT-bin/aether-distribution-0.10-SNAPSHOT/webr/docroot/
