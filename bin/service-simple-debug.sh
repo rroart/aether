e8b603c78cbcf8c8b9436bbf95188ee3a4e99b20
Roar Thronæs <roart@nvg.ntnu.no>
2022-04-06 22:09:09 +0200

    Variable injection for React frontend (I14).

--- bin/service-simple-debug.sh
+++ bin/service-simple-debug.sh
@@ -5,7 +5,7 @@ xterm -e "java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=18100,susp
 xterm -e "java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=18102,suspend=n -jar aether-core-0.10-SNAPSHOT.jar $1" &
 xterm -e "java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=18101,suspend=n -jar aether-web-0.10-SNAPSHOT.jar" &
 cd ../webr
-export REACT_APP_MYSERVER=MYSERVER
-export REACT_APP_MYPORT=MYPORT
+export REACT_APP_MYSERVER=$MYSERVER
+export REACT_APP_MYPORT=$MYPORT
 npx react-inject-env set -d docroot
 xterm -e "npx http-server docroot -p $WEBR" &
