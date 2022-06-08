ad293c2d4e26837a0c457e813a959e430907162d
Roar Thronæs <roart@nvg.ntnu.no>
2022-06-08 21:06:21 +0200

    Rename constants file, for refactor (I56).

---
+++ common/searchengine/src/main/java/roart/common/searchengine/SearchConstants.java
@@ -0,0 +1,14 @@
+package roart.common.searchengine;
+
+public interface SearchConstants {
+    String TITLE = "title";
+    String NAME = "name";
+    String LANG = "lang";
+    String METADATA = "metadata";
+    String MD5 = "id";
+    String ID = "id";
+    String CAT = "cat";
+    String CONTENT = "content";
+    String TEXT = "text";
+	String STOREHIGHLIGHT = "no";
+}
