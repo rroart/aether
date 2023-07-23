b9b038a569a4dfa333b38b21d54c66186787f4fb
Roar Thronæs <roart@nvg.ntnu.no>
2023-07-23 20:21:21 +0200

    Leader selection, for concurrency (I42).

---
+++ common/leader/src/main/java/roart/common/leader/impl/MyLeaderFactory.java
@@ -0,0 +1,23 @@
+package roart.common.leader.impl;
+
+import org.apache.curator.framework.CuratorFramework;
+
+import com.hazelcast.core.HazelcastInstance;
+
+import roart.common.config.MyConfig;
+import roart.common.leader.MyLeader;
+
+public class MyLeaderFactory {
+    public MyLeader create(String id, CuratorFramework curatorFramework, HazelcastInstance hz) {
+        if (MyConfig.conf.wantDistributedTraverse()) {
+            if (MyConfig.conf.getZookeeper() != null) {
+                return new MyCuratorLeader(id, curatorFramework, hz);
+            } else {
+                return new MyHazelcastLeader(id, curatorFramework, hz);
+            }
+        } else {
+            return null;
+        }
+    }
+
+}
