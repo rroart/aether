317ce069b088de99884d2fc999312b2739d86ed7
Roar Thronæs <roart@nvg.ntnu.no>
2023-07-23 23:06:19 +0200

    Unfinished deserialization, for concurrency (I42).

---
+++ common/util/src/main/java/roart/common/util/JsonData.java
@@ -0,0 +1,19 @@
+package roart.common.util;
+
+public class JsonData<T> {
+    private T result;
+
+    public JsonData() {
+        super();
+    }
+
+    public T getResult() {
+        return result;
+    }
+
+    public void setResult(T result) {
+        this.result = result;
+    }
+
+    
+}
