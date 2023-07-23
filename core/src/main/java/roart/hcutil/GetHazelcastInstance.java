936de5ebae1003bead9f1c96dd56e7869290d435
Roar Thronæs <roart@nvg.ntnu.no>
2023-07-23 15:01:21 +0200

    Hazelcast cluster name, for concurrency (I42).

--- core/src/main/java/roart/hcutil/GetHazelcastInstance.java
+++ core/src/main/java/roart/hcutil/GetHazelcastInstance.java
@@ -2,16 +2,24 @@ package roart.hcutil;
 
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
+
+import roart.common.constants.Constants;
+
 import com.hazelcast.config.Config;
 
 public class GetHazelcastInstance {
     static HazelcastInstance hz = null;
     public static synchronized HazelcastInstance instance() {
-	if (hz == null) {
-	    Config config = HazelcastConfig.getHazelcastConfig();
-	    config.getCPSubsystemConfig().setCPMemberCount(3);
-	    hz = Hazelcast.newHazelcastInstance(config);
-	}
-	return hz;
+        if (hz == null) {
+            Config config = HazelcastConfig.getHazelcastConfig();
+            String appid = System.getenv(Constants.APPID);
+            if (appid != null) {
+                config.setClusterName(appid);
+            }
+
+            //config.getCPSubsystemConfig().setCPMemberCount(3);
+            hz = Hazelcast.newHazelcastInstance(config);
+        }
+        return hz;
     }
 }
