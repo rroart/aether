35aa08f24873d44a13cbb50c347399cc1242b6dc
Roar Thronæs <roart@nvg.ntnu.no>
2023-09-17 21:01:38 +0200

    Extend work queue usage, for concurrency (I42).

--- core/src/main/java/roart/hcutil/GetHazelcastInstance.java
+++ core/src/main/java/roart/hcutil/GetHazelcastInstance.java
@@ -5,20 +5,27 @@ import com.hazelcast.core.HazelcastInstance;
 
 import roart.common.constants.Constants;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.hazelcast.config.Config;
 
 public class GetHazelcastInstance {
+    protected static Logger log = LoggerFactory.getLogger(GetHazelcastInstance.class);
     static HazelcastInstance hz = null;
-    public static synchronized HazelcastInstance instance() {
+    public static synchronized HazelcastInstance instance(String server) {
         if (hz == null) {
             Config config = HazelcastConfig.getHazelcastConfig();
+            config.getJetConfig().setEnabled(true);
+            // check
             String appid = System.getenv(Constants.APPID);
             if (appid != null) {
-                config.setClusterName(appid);
+                //config.setClusterName(appid);
             }
 
             //config.getCPSubsystemConfig().setCPMemberCount(3);
             hz = Hazelcast.newHazelcastInstance(config);
+            log.info("Creating Hazelcast instance");
         }
         return hz;
     }
