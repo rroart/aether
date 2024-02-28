ea0edc77f5f5df8b3d32e46fb1ea62eced09f868
Roar Thronæs <roart@nvg.ntnu.no>
2024-02-28 20:40:32 +0100

    Temp Hazelcast moveable lock, for concurrency (I42).

--- common/synchronization/src/main/java/roart/common/synchronization/impl/MyObjectLockFactory.java
+++ common/synchronization/src/main/java/roart/common/synchronization/impl/MyObjectLockFactory.java
@@ -8,6 +8,8 @@ import roart.common.synchronization.MyObjectLock;
 public class MyObjectLockFactory {
     public static MyObjectLock create(String name, String locker, CuratorFramework curatorFramework) {
         switch (locker) {
+        case Constants.HAZELCAST:
+            //return new MyHazelcastObjectLock(name, hz);
         case Constants.CURATOR:
             return new MyCuratorObjectLock(name, curatorFramework);
         }
